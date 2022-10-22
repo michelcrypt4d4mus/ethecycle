0cc56f1f73314125b3a7abd837c792fcbfaaece6
ashariyar <ashariyar@users.noreply.github.com>
2022-10-22 16:18:09 -0400

    load_txns

--- scripts/load_txns.py
+++ scripts/load_txns.py
@@ -1,6 +1,8 @@
+from ethecycle.graph import count_vertices, delete_graph
 from ethecycle.transaction_loader import load_txns_to_graph
 from ethecycle.util.string_constants import USDT_ADDRESS
 
 TRANSACTION_FILE = '/trondata/output_1000_lines.csv'
 
+delete_graph()
 load_txns_to_graph(TRANSACTION_FILE, USDT_ADDRESS)

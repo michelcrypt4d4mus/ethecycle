8ff662b8ec239c37bd5a53b7f5c2c97aea05fc4a
ashariyar <ashariyar@users.noreply.github.com>
2022-11-08 03:56:54 -0500

    query runs

--- scripts/queries/ftx_inbound_and_outbound_together.sql
+++ scripts/queries/ftx_inbound_and_outbound_together.sql
@@ -1,50 +1,65 @@
-WITH prices_by_minute AS (
-    SELECT
-      usd.contract_address,
-      usd.blockchain,
-      usd.minute,
-      usd.decimals,
-      AVG(usd.price) AS price
-    FROM prices.usd
-    WHERE usd.blockchain = 'ethereum'
-    GROUP BY 1,2,3,4
+-- Aggregate by minute/token/from_address
+WITH ftx_inbound AS (
+  SELECT
+    `from` AS from_address,
+    contract_address,
+    DATE_TRUNC('minute', evt_block_time) AS minute,
+    SUM(value) AS inbound_value,
+    COUNT(*) AS inbound_txn_count
+  FROM erc20_ethereum.evt_Transfer
+  WHERE `to` IN (
+      '0x2faf487a4414fe77e2327f0bf4ae2a264a776ad2',
+      '0xc098b2a3aa256d2140208c3de6543aaef5cd3a94',
+      '0x7abe0ce388281d2acf297cb089caef3819b13448'
+    )
+  GROUP BY 1,2,3
 ),
 
-ftx_inbound AS (
-    SELECT
-      `from` AS from_address,
-      SUM(prices_by_minute.price * value) / power(10, FIRST(prices_by_minute.decimals)) AS inbound_usd,
-      COUNT(*) AS inbound_txn_count
-    FROM erc20_ethereum.evt_Transfer
-      INNER JOIN prices_by_minute
-              ON prices_by_minute.contract_address = evt_Transfer.contract_address
-             AND prices_by_minute.minute = DATE_TRUNC('minute', evt_block_time)
-    WHERE `to` IN (
-        '0x2faf487a4414fe77e2327f0bf4ae2a264a776ad2',
-        '0xc098b2a3aa256d2140208c3de6543aaef5cd3a94',
-        '0x7abe0ce388281d2acf297cb089caef3819b13448'
-       )
-     GROUP BY 1
-     LIMIT 10000
+-- Convert to USD, aggregate just by from_address
+ftx_inbound_usd AS (
+  SELECT
+    from_address,
+    SUM(inbound_txn_count) AS inbound_txn_count,
+    SUM(usd.price * inbound_value) / power(10, FIRST(usd.decimals)) AS inbound_usd
+  FROM ftx_inbound
+    INNER JOIN prices.usd
+            ON usd.blockchain = 'ethereum'
+           AND usd.contract_address = ftx_inbound.contract_address
+           AND usd.minute = ftx_inbound.minute
+  GROUP BY 1
 ),
 
+-- Aggregate by minute/token/from_address
 ftx_outbound AS (
-    SELECT
-      `to` AS to_address,
-      SUM(prices_by_minute.price * value) / power(10, FIRST(prices_by_minute.decimals)) AS outbound_usd,
-      COUNT(*) AS outbound_txn_count
-    FROM erc20_ethereum.evt_Transfer
-      INNER JOIN prices_by_minute
-              ON prices_by_minute.contract_address = evt_Transfer.contract_address
-             AND prices_by_minute.minute = DATE_TRUNC('minute', evt_block_time)
-    WHERE `from` IN (
-        '0x2faf487a4414fe77e2327f0bf4ae2a264a776ad2',
-        '0xc098b2a3aa256d2140208c3de6543aaef5cd3a94',
-        '0x7abe0ce388281d2acf297cb089caef3819b13448'
-       )
-     GROUP BY 1
-     LIMIT 10000
+  SELECT
+    `to` AS to_address,
+    contract_address,
+    DATE_TRUNC('minute', evt_block_time) AS minute,
+    SUM(value) AS outbound_value,
+    COUNT(*) AS outbound_txn_count
+  FROM erc20_ethereum.evt_Transfer
+  WHERE `from` IN (
+    '0x2faf487a4414fe77e2327f0bf4ae2a264a776ad2',
+    '0xc098b2a3aa256d2140208c3de6543aaef5cd3a94',
+    '0x7abe0ce388281d2acf297cb089caef3819b13448'
+  )
+  GROUP BY 1,2,3
+),
+
+-- Convert to USD, aggregate just by from_address
+ftx_outbound_usd AS (
+  SELECT
+    to_address,
+    SUM(outbound_txn_count) AS outbound_txn_count,
+    SUM(usd.price * outbound_value) / power(10, FIRST(usd.decimals)) AS outbound_usd
+  FROM ftx_outbound
+    INNER JOIN prices.usd
+            ON usd.blockchain = 'ethereum'
+           AND usd.contract_address = ftx_outbound.contract_address
+           AND usd.minute = ftx_outbound.minute
+    GROUP BY 1
 )
+
 
 SELECT
   COALESCE(from_address, to_address) AS wallet_address,
@@ -62,8 +77,8 @@ SELECT
 
   -- Totals
   ROUND(COALESCE(outbound_usd, 0) + COALESCE(inbound_usd, 0)) AS gross_usd_volume
-FROM ftx_inbound
-  FULL OUTER JOIN ftx_outbound
-                ON ftx_outbound.to_address = ftx_inbound.from_address
+FROM ftx_inbound_usd
+  FULL OUTER JOIN ftx_outbound_usd
+                ON ftx_outbound_usd.to_address = ftx_inbound_usd.from_address
 ORDER BY gross_usd_volume DESC
 LIMIT 500

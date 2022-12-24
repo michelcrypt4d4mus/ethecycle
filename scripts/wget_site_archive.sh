afaa0f0dfd5bac660a50a336a97c8e977274076c
ashariyar <ashariyar@users.noreply.github.com>
2022-12-23 20:40:55 -0500

    Woop

--- scripts/wget_site_archive.sh
+++ scripts/wget_site_archive.sh
@@ -1,8 +1,8 @@
 # https://www.petekeen.net/archiving-websites-with-wget
+#  -- level is Recursion depth
 
 wget \
     --mirror \
-    --warc-file=silvergate \
     --warc-cdx \
     --page-requisites \
     --html-extension \
@@ -10,8 +10,12 @@ wget \
     --execute robots=off \
     --directory-prefix=. \
     --span-hosts \
-    --domains=example.com,www.example.com,cdn.example.com \
     --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15' \
     --wait=10 \
     --random-wait \
-    https://www.silvergate.com
+    --recursive \
+    --no-parent \
+    --warc-file=docs.strikeprotocols.com \
+    --domains=docs.strikeprotocols.com \
+    --level=5 https://docs.strikeprotocols.com
+

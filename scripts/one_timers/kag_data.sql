4684e01d946dcb067983f304ee7463c1ed833e0c
ashariyar <ashariyar@users.noreply.github.com>
2023-01-02 12:49:38 -0500

    KAG balances

--- scripts/one_timers/kag_data.sql
+++ scripts/one_timers/kag_data.sql
@@ -1,5 +1,4 @@
 -- created_at,id,type,memo,from,to,amount,fee,token,successful
-
 CREATE TABLE kag_txns (
   created_at TIMESTAMPTZ,
   id VARCHAR NOT NULL UNIQUE,
@@ -14,6 +13,7 @@ CREATE TABLE kag_txns (
 );
 
 
+-- Load data
 \copy kag_txns
   FROM '/Users/uz0r/Downloads/KAG.csv'
     CSV
@@ -33,6 +33,7 @@ FROM kag_txns
 GROUP BY 1
 ORDER BY 2 DESC;
 
+
 -- Biggest receivers
 SELECT
   to_address,
@@ -44,3 +45,40 @@ SELECT
 FROM kag_txns
 GROUP BY 1
 ORDER BY 2 DESC;
+
+
+-- Current balances
+WITH sent_amounts AS (
+  SELECT
+    from_address,
+    ROUND(SUM(amount)::NUMERIC, 2) AS sent_amount,
+    ROUND(SUM(fee)::NUMERIC, 2) AS sent_fee,
+    COUNT(*) AS sent_txn_count
+  FROM kag_txns
+  GROUP BY 1
+),
+
+received_amounts AS (
+    SELECT
+      to_address,
+      ROUND(SUM(amount)::NUMERIC, 2) AS received_amount,
+      COUNT(*) AS received_txn_count
+    FROM kag_txns
+    GROUP BY 1
+)
+
+SELECT
+  COALESCE(to_address, from_address) AS address,
+  COALESCE(received_amount, 0.0) - COALESCE(sent_amount, 0.0) - COALESCE(sent_fee, 0.0) AS current_balance,
+  COALESCE(received_amount, 0.0) + COALESCE(sent_amount, 0.0) AS gross_txn_amount,
+  COALESCE(received_amount, 0.0) AS in_amount,
+  COALESCE(sent_amount, 0.0) AS out_amount,
+  COALESCE(sent_fee, 0.0) AS fees,
+  COALESCE(sent_txn_count, 0) AS out_txn_count,
+  COALESCE(received_txn_count, 0) AS in_txn_count,
+  COALESCE(sent_txn_count, 0) + COALESCE(received_txn_count, 0) AS gross_txn_count
+FROM received_amounts
+  FULL OUTER JOIN sent_amounts
+               ON received_amounts.to_address = sent_amounts.from_address
+ORDER BY 2 DESC
+

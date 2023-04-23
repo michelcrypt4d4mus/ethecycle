82a9925aef6bc8d7a77ee9b92faeca43cc8ed7a6
ashariyar <ashariyar@users.noreply.github.com>
2023-04-22 23:26:23 -0400

    more lost address massaging

--- ethecycle/chain_addresses/importers/lost_forever_addresses_importer.py
+++ ethecycle/chain_addresses/importers/lost_forever_addresses_importer.py
@@ -6,22 +6,18 @@ import csv
 from os import path
 from typing import List
 
-from rich.panel import Panel
 
-from ethecycle.blockchains.blockchains import CHAIN_IDS
 from ethecycle.chain_addresses.address_db import insert_addresses
 from ethecycle.chain_addresses.github_data_source import GithubDataSource
-from ethecycle.models.blockchain import guess_chain_info_from_address
-from ethecycle.models.token import Token
 from ethecycle.models.wallet import Wallet
-from ethecycle.util.dict_helper import walk_json
-from ethecycle.util.filesystem_helper import files_in_dir, get_lines, load_file_contents, subdirs_of_dir
-from ethecycle.util.logging import console, log, print_address_import
+from ethecycle.util.filesystem_helper import get_lines
+from ethecycle.util.logging import log
 from ethecycle.util.string_constants import *
 
 LOST_ETH_REPO = GithubDataSource('jconorgrogan/Lost-ETH')
 LOST_BTC_REPO = GithubDataSource('jconorgrogan/BTC-Burn-Addresses')
 BTC_ADDRESS_LINE_PREFIX = '- '
+LOST = 'lost'
 
 
 def import_lost_forever_addresses():
@@ -37,7 +33,7 @@ def import_lost_forever_addresses():
                 blockchain='bitcoin',
                 address=line.removeprefix(BTC_ADDRESS_LINE_PREFIX).strip(),
                 name='Lost forever because there is no private key owner',
-                category='lost',
+                category=LOST,
                 data_source=LOST_BTC_REPO.repo_url
             )
 
@@ -50,21 +46,31 @@ def import_lost_forever_addresses():
     with LOST_ETH_REPO.local_repo_path() as repo_dir:
         with open(path.join(repo_dir, 'Lost Eth.csv'), newline='') as csvfile:
             for row in csv.DictReader(csvfile, delimiter=","):
-                if row['Address'] == '0x0000000000000000000000000000000000000000':
+                address = row['Address']
+                row_comment = row['Comment']
+
+                if address == '0x0000000000000000000000000000000000000000':
                     continue
 
-                if row['Type'].startswith('Parity'):
-                    organization = 'Parity'
-                    comment = 'Funds lost forever: https://techcrunch.com/2017/12/05/parity-ceo-says-shes-confident-that-its-280m-in-frozen-ethereum-isnt-lost-forever/'
+                organization = None
+                category = None
+                comment = None
 
-                    if row['Comment'] == '':
-                        name = row['Type']
+                if row['Type'].startswith('Parity'):
+                    comment = 'Funds lost to Parity wallet disaster'
+
+                    if row_comment == '':
+                        name = row['Type'] + ' (lost forever)'
+                        organization = 'Parity'
                     else:
-                        name = f"{row['Comment']} {row['Type']}"
+                        name = f"{row_comment} {row['Type']} ({LOST} forever)"
+                        organization = row_comment
+                elif address.startswith('0x00000000000000000000000000000000000'):
+                    name = 'Burn address'
+                    category = 'burn'
                 else:
                     name=f"Funds lost forever due to {row['Type']}"
-                    organization = None
-                    row_comment = row['Comment']
+                    row_comment = row_comment
 
                     if 'Pfeffer' in row_comment:
                         comment = 'From query by Johannes Pfeffer'
@@ -74,10 +80,10 @@ def import_lost_forever_addresses():
                         comment = row_comment
 
                 wallet = Wallet(
-                    address=row['Address'],
-                    blockchain='ethereum',
+                    address=address,
+                    blockchain=ETHEREUM,
                     name=name,
-                    category='lost',
+                    category=category or LOST,
                     comment=comment,
                     organization=organization,
                     data_source = LOST_ETH_REPO.repo_url
